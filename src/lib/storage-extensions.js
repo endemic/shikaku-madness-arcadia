22e0ceeabba815d2cf0ea743e7d0f441f2f68fa0
Nathan Demick <ndemick@gmail.com>
2015-08-14 07:23:51 -0400

    Update localStorage to namespace keys
    Rationale for this is that if I host similar games on the same domain,
    they have a chance of overwriting each others' data.

--- src/lib/storage-extensions.js
+++ src/lib/storage-extensions.js
@@ -1,19 +1,42 @@
-Storage.prototype.setObject = function (key, value) {
-    this.setItem(key, JSON.stringify(value));
-};
+(function () {
+    Storage.namespace = 'shikaku';
 
-Storage.prototype.getObject = function (key) {
-    var value = this.getItem(key);
-    if (value) {
-        return JSON.parse(value);
-    }
-    return null;
-};
+    var originalGetItem = Storage.prototype.getItem,
+        originalSetItem = Storage.prototype.setItem;
 
-Storage.prototype.setBoolean = function (key, value) {
-    this.setItem(key, !!value);
-};
+    Storage.prototype.setItem = function (key, value) {
+        key = this.namespace(key);
+        return originalSetItem.apply(this, arguments);
+    };
 
-Storage.prototype.getBoolean = function (key) {
-    return this.getItem(key) == 'true';
-};
+    Storage.prototype.getItem = function (key) {
+        key = this.namespace(key);
+        return originalGetItem.apply(this, arguments);
+    };
+
+    Storage.prototype.namespace = function (key) {
+        return Storage.namespace + ':' + key;
+    };
+
+    Storage.prototype.setObject = function (key, value) {
+        this.setItem(key, JSON.stringify(value));
+    };
+
+    Storage.prototype.getObject = function (key) {
+        var value = this.getItem(key);
+
+        if (value) {
+            return JSON.parse(value);
+        }
+
+        return null;
+    };
+
+    Storage.prototype.setBoolean = function (key, value) {
+        this.setItem(key, !!value);
+    };
+
+    Storage.prototype.getBoolean = function (key) {
+        return this.getItem(key) == 'true';
+    };
+})();
