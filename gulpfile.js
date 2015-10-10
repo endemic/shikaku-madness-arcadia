c7b592a5e6677c6125a8949e487d64309bbe676e
Nathan Demick <ndemick@gmail.com>
2015-10-09 20:47:04 -0400

    copy over gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+var gulp = require('gulp'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename');
+
+gulp.task('default', function () {
+    console.log('stuff');
+});
+
+gulp.task('concat', function () {
+    var src = [
+        'src/data/*.js',
+        'src/lib/*.js',
+        'src/objects/*.js',
+        'src/scenes/*.js'
+    ];
+    return gulp.src(src).pipe(concat('shikaku-madness.js'))
+                        .pipe(gulp.dest('dist'));
+});
+
+
+gulp.task('compress', function () {
+    // TODO: source map
+    return gulp.src('dist/shikaku-madness.js')
+               .pipe(uglify())
+               .pipe(rename('shikaku-madness.min.js'))
+               .pipe(gulp.dest('dist'));
+});
+
+gulp.task('cordova', ['concat', 'compress'], function () {
+    gulp.src(['dist/shikaku-madness.min.js'], { base: 'dist' })
+        .pipe(gulp.dest('cordova/www/js'));
+
+    gulp.src(['node_modules/arcadia/dist/arcadia.js'], { base: 'node_modules/arcadia/dist' })
+        .pipe(gulp.dest('cordova/www/js'));
+
+    gulp.src(['assets/**'])
+        .pipe(gulp.dest('cordova/www/assets'));
+});
