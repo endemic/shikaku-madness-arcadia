adf3faa6015be6997047b823d06e8eb0992c873e
Nathan Demick <ndemick@gmail.com>
2015-10-18 10:50:00 -0400

    Add rules scene

---
+++ src/scenes/rules.js
@@ -0,0 +1,70 @@
+/*jslint sloppy: true */
+/*globals Arcadia, TitleScene, localStorage, window, sona */
+
+var RulesScene = function () {
+    Arcadia.Scene.apply(this);
+
+    var titleLabel,
+        backButton,
+        detailLabel,
+        text;
+
+    text = [
+        'Tap + drag on the puzzle\n',
+        'grid to draw. Each number\n',
+        'on the grid is a clue; the\n',
+        'number represents the size\n',
+        'of the rectangle that\n',
+        'overlaps it. For example,\n',
+        'a clue of "9" means that\n',
+        'you need to draw a 3x3\n',
+        'square on top of that\n',
+        'clue. The trick is to draw\n',
+        'each square and rectangle\n',
+        'so that there\'s no wasted\n',
+        'space on the grid. You\n',
+        'can\'t overlap squares,\n',
+        'either. Fortunately,\n',
+        'there\'s no time limit,\n',
+        'and you can always try\n',
+        'again if you make a\n',
+        'mistake.'
+    ];
+
+    titleLabel = new Arcadia.Label({
+        text: 'Rules',
+        font: '64px monospace',
+        position: { x: 0, y: -this.size.height / 2 + 110 }
+    });
+    this.add(titleLabel);
+
+    backButton = new Arcadia.Button({
+        position: { x: -this.size.width / 2 + 65, y: -this.size.height / 2 + 25 },
+        color: null,
+        border: '3px white',
+        label: new Arcadia.Label({
+            text: '<- title',
+            color: 'white',
+            font: '24px monospace'
+        }),
+        action: function () {
+            sona.play('button');
+            Arcadia.changeScene(TitleScene);
+        }
+    });
+    this.add(backButton);
+
+    detailLabel = new Arcadia.Label({
+        text: text.join(''),
+        alignment: 'left',
+        font: '24px monospace',
+        position: {
+            x: 0,
+            y: 75
+        }
+    });
+    this.add(detailLabel);
+
+};
+
+RulesScene.prototype = new Arcadia.Scene();
