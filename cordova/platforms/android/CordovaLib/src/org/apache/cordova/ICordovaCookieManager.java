8fe8650552daedbbffa40915f44774917a4319fb
Nathan Demick <ndemick@gmail.com>
2016-05-02 21:25:34 -0400

    Updates to Android project

--- cordova/platforms/android/CordovaLib/src/org/apache/cordova/ICordovaCookieManager.java
+++ cordova/platforms/android/CordovaLib/src/org/apache/cordova/ICordovaCookieManager.java
@@ -9,4 +9,25 @@
 
          http://www.apache.org/licenses/LICENSE-2.0
 
-     
+       Unless required by applicable law or agreed to in writing,
+       software distributed under the License is distributed on an
+       "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+       KIND, either express or implied.  See the License for the
+       specific language governing permissions and limitations
+       under the License.
+*/
+
+package org.apache.cordova;
+
+public interface ICordovaCookieManager {
+
+    public void setCookiesEnabled(boolean accept);
+
+    public void setCookie(final String url, final String value);
+
+    public String getCookie(final String url);
+
+    public void clearCookies();
+
+    public void flush();
+};
--- cordova/platforms/ios/www/cordova_plugins.js
+++ cordova/platforms/ios/www/cordova_plugins.js
@@ -1,14 +1,6 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
-        "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
-        "id": "cordova-plugin-splashscreen.SplashScreen",
-        "pluginId": "cordova-plugin-splashscreen",
-        "clobbers": [
-            "navigator.splashscreen"
-        ]
-    },
-    {
         "file": "plugins/cc.fovea.cordova.purchase/www/store-ios.js",
         "id": "cc.fovea.cordova.purchase.InAppPurchase",
         "pluginId": "cc.fovea.cordova.purchase",
@@ -31,15 +23,23 @@ module.exports = [
         "clobbers": [
             "cordova.logger"
         ]
+    },
+    {
+        "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
+        "id": "cordova-plugin-splashscreen.SplashScreen",
+        "pluginId": "cordova-plugin-splashscreen",
+        "clobbers": [
+            "navigator.splashscreen"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.2.2",
-    "cordova-plugin-splashscreen": "2.0.0",
     "cc.fovea.cordova.purchase": "4.0.0",
-    "cordova-plugin-console": "1.0.3"
+    "cordova-plugin-console": "1.0.3",
+    "cordova-plugin-whitelist": "1.2.2",
+    "cordova-plugin-splashscreen": "2.0.0"
 }
 // BOTTOM OF METADATA
 });
